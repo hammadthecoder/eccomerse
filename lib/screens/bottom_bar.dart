b7e7ad64f28f8e30436f4f5e151f358927ab255b
Hammad Yousuf <hammadbhat126@gmail.com>
2022-01-13 08:12:53 +0400

    bottom navigitaion

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class BottomBar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     throw UnimplementedError();
   }
   
